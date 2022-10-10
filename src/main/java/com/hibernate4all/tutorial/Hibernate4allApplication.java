e36c68992a86ec0ebdbd2eca98494b4dc00bec9f
ToureMoustapha <moustapha.toure@etu.univ-amu.fr>
2022-10-10 19:36:28 +0000

    add src files

---
+++ src/main/java/com/hibernate4all/tutorial/Hibernate4allApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate4all.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate4allApplication.class, args);
+	}
+
+}
